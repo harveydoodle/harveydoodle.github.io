3ffb4dace9e8434ec5410324e3a4bfeedd392998
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-14 14:50:21 -0400

    node servre

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 
 //Lets require/import the HTTP module
-var http = require('http');
-var path = require("path");
+let http = require('http');
+let path = require("path");
+var express = require('express');
+var app = express();
 
 //Lets define a port we want to listen to
 // const PORT=8080; 
-const PORT = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 8080));
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+// function handleRequest(request, response){
+//     response.end('It Works!! Path Hit: ' + request.url);
+// }
 
-//Create a server
-// var server = http.createServer(handleRequest);
-var server = http.createServer(function(request, response) {
-  var filePath = false;
+app.use(express.static(__dirname + '/public'));
 
-  if (request.url == '/') {
-    filePath = "./index.html";
-  } 
-  else {
-    filePath = "index.html";
-  }
-
-//   var absPath = "./" + filePath;
-//   serverWorking(response, absPath);
+app.get('/', function(request, response) {
+  response.render('index.html')
 });
 
 
+//Create a server
+// var server = http.createServer(handleRequest);
+// var server = http.createServer(function(request, response) {
+//   var filePath = false;
+
+//   if (request.url == '/') {
+//     filePath = "./index.html";
+//   } 
+//   else {
+//     filePath = "index.html";
+//   }
+
+//   var absPath = "./" + filePath;
+//   serverWorking(response, absPath);
+// });
+
+
 //Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+// server.listen(PORT, function(){
+//     //Callback triggered when server is successfully listening. Hurray!
+//     console.log("Server listening on: http://localhost:%s", PORT);
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
