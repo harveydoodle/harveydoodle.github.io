f5d58e801a92c56dcc08c30a757d04f58cfe68c0
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-14 00:45:50 -0400

    test

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+{<h1>asdf</h1>}
