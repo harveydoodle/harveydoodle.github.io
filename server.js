17ab47e44acc5852d9e75a935d75df98973c6f59
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-14 00:54:38 -0400

    test

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-// document.write('asdf');
-
-console.log('hi');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
